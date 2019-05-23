9889c32e4c1bfb7c2516536a929b2f9870eab170
KevinOlvera <kevin.jesus.olvera@gmail.com>
2019-05-23 11:47:27 -0500

    add menu

--- LLC_Analyser.c
+++ LLC_Analyser.c
@@ -2,6 +2,35 @@
 
 int main(int argc, char const *argv[])
 {
-    Read_File("tramas.txt");
+    char option;
+    int flag = 1;
+
+    printf("Analizador de tramas LLC\n");
+    printf(" 1.- Analizar tramas del archivo\n 2.- Analizar trama de la red\n 3.- Salir\n");
+    while(flag)
+    {
+        scanf("%c", &option);
+        if(option != ' ' || option != '\n')
+            flag = 0;
+    }
+
+    switch (option)
+    {
+        case '1':    
+            Read_File("tramas.txt");
+        break;
+
+        case '2':
+        break;
+
+        case '3':
+            flag = 0;
+        break;
+        
+        default:
+            printf("Introduce una opcion valida");
+        break;
+    }
+
     return 0;
 }
